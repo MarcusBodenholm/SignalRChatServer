c3af0df18c3d1c8d5d3e75f4523586cad696992c
Marcus Bodenholm <shedcapeyt@gmail.com>
2024-09-27 12:25:37 +0200

    Lagt till logging i AuthController.

--- SignalRChatServer/Controllers/AuthController.cs
+++ SignalRChatServer/Controllers/AuthController.cs
@@ -12,37 +12,67 @@ public class AuthController : Controller
     private readonly ChatContext _context;
     private readonly IConfiguration _config;
     private readonly IAuthServices _authService;
+    private readonly ILogger _logger;
 
-    public AuthController(ChatContext context, IConfiguration config, IAuthServices authServices)
+    public AuthController(ChatContext context, IConfiguration config, IAuthServices authServices, ILogger logger)
     {
         _context = context;
         _config = config;
         _authService = authServices;
+        _logger = logger;
     }
     [HttpPost("signup")]
     public async Task<IActionResult> Signup([FromBody] UserDto userDto)
     {
-        if (userDto.Password != userDto.ConfirmPassword)
+        _logger.LogInformation("User attempts signup");
+        try
         {
-            return BadRequest(new { message = "Passwords do not match" });
+            if (userDto.Password != userDto.ConfirmPassword)
+            {
+                _logger.LogInformation("User signup failed due to passwords not matching.");
+
+                return BadRequest(new { message = "Passwords do not match" });
+            }
+            var hashedPassword = BCrypt.Net.BCrypt.HashPassword(userDto.Password);
+            var user = new User { Username = userDto.Username, PasswordHash = hashedPassword };
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+            _logger.LogInformation("User signup successful.");
+            return Ok("User registered successfully.");
+
         }
-        var hashedPassword = BCrypt.Net.BCrypt.HashPassword(userDto.Password);
-        var user = new User { Username = userDto.Username, PasswordHash = hashedPassword };
-        _context.Users.Add(user);
-        await _context.SaveChangesAsync();
-        return Ok("User registered successfully.");
+        catch (Exception ex)
+        {
+            _logger.LogError($"ERROR AuthController > Signup :: {ex.Message}");
+            return new StatusCodeResult(StatusCodes.Status500InternalServerError);
+
+        }
     }
     [HttpPost("login")]
     public async Task<IActionResult> Login([FromBody] LoginDto loginDto)
     {
-        var user = await _context.Users.SingleOrDefaultAsync(u => u.Username == loginDto.Username);
+        _logger.LogInformation("User attempts login");
 
-        if (user == null || !BCrypt.Net.BCrypt.Verify(loginDto.Password, user.PasswordHash))
+        try
         {
-            return Unauthorized("The username or password was incorrect.");
+            var user = await _context.Users.SingleOrDefaultAsync(u => u.Username == loginDto.Username);
+
+            if (user == null || !BCrypt.Net.BCrypt.Verify(loginDto.Password, user.PasswordHash))
+            {
+                _logger.LogInformation("Login failed due to incorrect username or password.");
+                return Unauthorized("The username or password was incorrect.");
+            }
+            var secretKey = _config["SecretKey"];
+            var token = _authService.GenerateJwtToken(user, secretKey);
+            _logger.LogInformation("Login successful");
+            return Ok(new { Token = token });
+
         }
-        var secretKey = _config["SecretKey"];
-        var token = _authService.GenerateJwtToken(user, secretKey);
-        return Ok(new { Token = token });
+        catch (Exception ex)
+        {
+            _logger.LogError($"ERROR AuthController > Login :: {ex.Message}");
+            return new StatusCodeResult(StatusCodes.Status500InternalServerError);
+
+        }
     }
 }
